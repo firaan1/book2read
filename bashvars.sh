cc25262062bb58c01f37c19f66cacd0bbfb7fc37
firaan1 <firaan1@gmail.com>
2018-10-09 21:01:48 +0200

    db

--- bashvars.sh
+++ bashvars.sh
@@ -2,5 +2,4 @@
 export FLASK_APP=application.py
 export FLASK_DEBUG=1
 export DATABASE_URL='postgres://vjdgcgyzjslszt:6dc70b334fc246ab13fbab27901b0c1e23e579a355fdb2ca6e2aecf917e7e348@ec2-54-217-245-26.eu-west-1.compute.amazonaws.com:5432/dan8jdmklvmt96'
-#export DATABASE_URL='postgresql://project1:password@127.0.0.1:5432/project1db'
 export key="FNNcHf070O64hLKODokItA"
